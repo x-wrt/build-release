63642720f9446c409050e93c7acae3e20bdb2f36
Chen Minqiang <ptpt52@gmail.com>
2019-11-29 14:26:57 +0800

    build less

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 
 CFGS="
-config.ar71xx-generic
-config.ar71xx-generic-nosymbol
-config.ar71xx-nand
-config.ar71xx-nand-nosymbol
-config.ar71xx-tiny
 config.ath79-generic
 config.ath79-generic-nosymbol
 config.ath79-nand
@@ -13,13 +8,20 @@ config.ramips-mt7620-nosymbol
 config.ramips-mt7621
 config.ramips-mt76x8
 config.ramips-mt76x8-nosymbol
-config.ramips-rt3883-nosymbol
-config.ramips-rt305x
 config.ipq40xx-generic
-config.ipq806x-generic
 "
 
 export CFGS="`echo $CFGS`"
+export TMPFS="1"
+
+echo starting build.
+echo starting build..
+echo starting build...
+echo starting build....
+df -h .
+free -m
+echo start build in 10s
+sleep 10
 
 mkdir .build_x
 echo CONFIG_VERSION_NUMBER=\"`cat release.tag`\" >.build_x/env
