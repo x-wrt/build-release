cf9d06d4f7a7e3c6ed97a5cf6d1e9a8826c43256
Chen Minqiang <ptpt52@gmail.com>
2019-11-29 08:17:26 +0800

    release: 6.0_b201911290805
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

--- update-tags.sh
+++ update-tags.sh
@@ -3,5 +3,4 @@ TAG=`cat release.tag`
 
 sed -i "s/name: build-x-wrt-.*/name: build-x-wrt-$TAG/g" .github/workflows/main.yml && \
 	git commit --signoff -am "release: $TAG" && \
-	git tag $TAG && \
-	git push origin $TAG || exit 1
+	git push origin master || exit 1
