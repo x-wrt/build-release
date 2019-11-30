82cff2f68bcbaa352f0b0eb66d6cb758bc07e25a
Chen Minqiang <ptpt52@gmail.com>
2019-12-01 02:22:40 +0800

    Create Release Assets

--- update-tags.sh
+++ update-tags.sh
@@ -4,9 +4,11 @@ cat target.list | while read target; do
 		echo $target | grep -q "$1" || continue
 	}
 	TAG="`cat release.tag`""`echo -n $target | sed 's/TARGET//'`"
+	TAGNAME="`echo -n $TAG | sed 's/=/-/g'`"
 	echo push $TAG
 
-	sed -i "s/name: x-wrt-.*=.*/name: x-wrt-$TAG/g" main.yml && \
+	sed -i "s/name: x-wrt-.*=.*/name: x-wrt-$TAG/" main.yml && \
+		sed -i "s/asset_name: x-wrt-.*-.*\.zip/asset_name: x-wrt-$TAGNAME\.zip/" main.yml && \
 		sed -i "s/TARGET=.* sh /$target sh /" main.yml && \
 		cp main.yml .github/workflows/$target.yml
 done
