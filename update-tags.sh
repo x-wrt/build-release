fdeba9df52517f9c641bca022589a187268b2df5
Chen Minqiang <ptpt52@gmail.com>
2019-11-30 17:50:35 +0800

    short name

--- update-tags.sh
+++ update-tags.sh
@@ -3,10 +3,10 @@ cat target.list | while read target; do
 	test -n "$1" && {
 		echo $target | grep -q "$1" || continue
 	}
-	TAG="`cat release.tag`-$target"
+	TAG="`cat release.tag`""`echo -n $target | sed 's/TARGET//'`"
 	echo push $TAG
 
-	sed -i "s/name: x-wrt-.*/name: x-wrt-$TAG/g" main.yml && \
+	sed -i "s/name: x-wrt-.*=.*/name: x-wrt-$TAG/g" main.yml && \
 		sed -i "s/TARGET=.* sh /$target sh /" main.yml && \
 		cp main.yml .github/workflows/$target.yml
 done
