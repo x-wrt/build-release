fac3a4d911505436a31ad0780f8507572e3e146d
Chen Minqiang <ptpt52@gmail.com>
2019-12-02 10:39:36 +0800

    create release before build

--- update-tags.sh
+++ update-tags.sh
@@ -1,7 +1,5 @@
 
 change=
-NEEDS=
-ASSET_NAMES=
 
 for target in `cat target.list`; do
 	test -n "$1" && {
@@ -10,26 +8,17 @@ for target in `cat target.list`; do
 	TAG="`cat release.tag`"
 	TARGET="`echo -n $target | sed 's/TARGET=//'`"
 	ASSET_NAME="x-wrt-${TAG}-${TARGET}"
+	ASSET_ID=`echo ${ASSET_NAME} | tr \. -`
 	echo push ${ASSET_NAME}
 
 	test -n "$change" || {
 		cat release-header.yml | sed "s/_TAG_/${TAG}/g" >.github/workflows/release.yml
 		change=1
 	}
-	cat release-main.yml | sed "s/_TARGET_/${TARGET}/g;s/_ASSET_NAME_/${ASSET_NAME}/g" >>.github/workflows/release.yml
-	NEEDS="$NEEDS build-${TARGET}"
-	ASSET_NAMES="${ASSET_NAMES} ${ASSET_NAME}"
+	cat release-main.yml | sed "s/_TARGET_/${TARGET}/g;s/_ASSET_NAME_/${ASSET_NAME}/g;s/_ASSET_ID_/${ASSET_ID}/g" >>.github/workflows/release.yml
 done
 
 test -n "$change" || exit 0
-
-NEEDS=`echo -n $NEEDS | sed 's/ /,/g'`
-cat release-post.yml | sed "s/_NEEDS_/[${NEEDS}]/g" >>.github/workflows/release.yml
-
-for ASSET_NAME in ${ASSET_NAMES}; do
-	ASSET_ID=`echo ${ASSET_NAME} | tr \. -`
-	cat release-end.yml | sed "s/_ASSET_NAME_/${ASSET_NAME}/g;s/_ASSET_ID_/${ASSET_ID}/g" >>.github/workflows/release.yml
-done
 
 TAG="v`cat release.tag`"
 git commit --signoff -am "release: $TAG" && \
