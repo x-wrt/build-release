6b0a0df4f7e09b5e39c815645037aa8e6efb74dd
Developer X <dev@x-wrt.com>
2025-06-23 13:08:15 +0800

    release: v25.04_b202506231258
    Signed-off-by: Developer X <dev@x-wrt.com>

--- upload.sh
+++ upload.sh
@@ -8,6 +8,7 @@ sh feeds/x/rom/lede/gen_map.sh
 echo gen zip
 mkdir -p rom/sdk
 cp `find $(cat upload.list | cut -d/ -f1,2,3,4 | sort | uniq) | grep kmod-mtd-rw` rom
+cp `find $(cat upload.list | cut -d/ -f1,2 | sort | uniq) | grep luci-theme-argon` rom
 mv `cat upload.list` map.list sha256sums.txt rom
 mv `cat sdk_upload.list` sdk_map.list sdk_sha256sums.txt rom/sdk
 sh -c "cd rom && sh ../gen_index.sh"
