0a5359dc1aa97d7298b18fc9f8ccd30d6e38a836
Developer X <dev@x-wrt.com>
2025-07-27 22:12:02 +0800

    release: v25.04_b202507270846
    Signed-off-by: Developer X <dev@x-wrt.com>

--- upload.sh
+++ upload.sh
@@ -9,8 +9,8 @@ mkdir rom
 cp `find bin | grep kmod-mtd-rw` rom || true
 cp `find bin | grep luci-theme-argon` rom || true
 cp `find bin | grep openlist` rom || true
-echo gen x-wrt-${CONFIG_VERSION_NUMBER}-${cfg##config.}-apps.zip
-zip -r x-wrt-${CONFIG_VERSION_NUMBER}-${cfg##config.}-apps.zip rom && rm -rf rom || exit 255
+echo gen x-wrt-${cfg##config.}-apps.zip
+zip -r x-wrt-${cfg##config.}-apps.zip rom && rm -rf rom || exit 255
 
 echo gen zip
 cat .build_x/$cfg | grep "^CONFIG_TARGET_DEVICE_[^P]*_DEVICE_.*=y" | awk '{print length(), $0 | "sort -n -r"}' | cut -d' ' -f2 | sed 's/_DEVICE_/ /g;s/=y//' | while read _ target device; do
@@ -22,8 +22,8 @@ cat .build_x/$cfg | grep "^CONFIG_TARGET_DEVICE_[^P]*_DEVICE_.*=y" | awk '{print
 	cat sha256sums.txt | grep "${target}-${device}-" >rom/sha256sums.txt
 	mv `cat rom/upload.list` rom
 	sh -c "cd rom && sh ../gen_index.sh"
-	echo gen x-wrt-${CONFIG_VERSION_NUMBER}-${cfg##config.}-${device}.zip
-	zip -r x-wrt-${CONFIG_VERSION_NUMBER}-${cfg##config.}-${device}.zip rom && rm -rf rom || exit 255
+	echo gen x-wrt-${cfg##config.}-${device}.zip
+	zip -r x-wrt-${cfg##config.}-${device}.zip rom && rm -rf rom || exit 255
 done
 
 done
