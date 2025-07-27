ddd4dee194bc732baae47d2ba2a042c6a8b6b483
Developer X <dev@x-wrt.com>
2025-07-27 14:02:48 +0800

    release: v25.04_b202507270846
    Signed-off-by: Developer X <dev@x-wrt.com>

--- upload.sh
+++ upload.sh
@@ -13,13 +13,20 @@ cat .build_x/$cfg | grep "^CONFIG_TARGET_DEVICE_[^P]*_DEVICE_.*=y" | sed 's/_DEV
 	echo target=$target device=$device
 	head -n3 map.list
 	head -n3 upload.list
-	cp `find $(cat upload.list | cut -d/ -f1,2,3,4 | sort | uniq) | grep kmod-mtd-rw` rom
-	cp `find $(cat upload.list | cut -d/ -f1,2 | sort | uniq) | grep luci-theme-argon` rom
-	cp `find $(cat upload.list | cut -d/ -f1 | sort | uniq) | grep openlist` rom
-	cat map.list | grep "${target/_/-}-${device}-" >rom/map.list
-	cat upload.list | grep "${target/_/-}-${device}-" >rom/upload.list
-	cat sha256sums.txt | grep "${target/_/-}-${device}-" >rom/sha256sums.txt
-	mv `cat rom/upload.list` rom
+	ls rom
+	ls -l
+	cat map.list | grep "${target/_/-}-${device}-"
+	set -x
+	cat map.list | grep "${target/_/-}-${device}-"
+	set +x
+	echo
+	cp `find $(cat upload.list | cut -d/ -f1,2,3,4 | sort | uniq) | grep kmod-mtd-rw` rom || true
+	cp `find $(cat upload.list | cut -d/ -f1,2 | sort | uniq) | grep luci-theme-argon` rom || true
+	cp `find $(cat upload.list | cut -d/ -f1 | sort | uniq) | grep openlist` rom || true
+	cat map.list | grep "${target/_/-}-${device}-" > ./rom/map.list
+	cat upload.list | grep "${target/_/-}-${device}-" > ./rom/upload.list
+	cat sha256sums.txt | grep "${target/_/-}-${device}-" > ./rom/sha256sums.txt
+	mv `cat rom/upload.list` ./rom/
 	sh -c "cd rom && sh ../gen_index.sh"
 	echo gen x-wrt-${CONFIG_VERSION_NUMBER}-${cfg##config.}-${device}.zip
 	zip -r x-wrt-${CONFIG_VERSION_NUMBER}-${cfg##config.}-${device}.zip rom && rm -rf rom || exit 255
