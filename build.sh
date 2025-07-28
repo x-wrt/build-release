420231517528fa8c541ea58f2bf45acdd55064bc
Developer X <dev@x-wrt.com>
2025-07-28 22:31:41 +0800

    release: v25.04_b202507270846
    Signed-off-by: Developer X <dev@x-wrt.com>

--- build.sh
+++ build.sh
@@ -20,10 +20,20 @@ sleep 10
 mkdir .build_x
 echo CONFIG_VERSION_NUMBER=\"`cat release.tag`\" >.build_x/env
 sed -i 's/CONFIG_SDK/# CONFIG_SDK is not set/' ./feeds/x/rom/lede/config.*
+
+sudo -E mkdir /data_dir && sudo -E chmod 777 /data_dir
+mkdir -p /data_dir/staging_dir && ln -s /data_dir/staging_dir staging_dir
+mkdir -p /data_dir/bin && ln -s /data_dir/bin bin
+mkdir -p /data_dir/dl && ln -s /data_dir/dl dl
+mkdir -p /data_dir/host && mkdir -p build_dir && ln -s /data_dir/host build_dir/host
+mkdir -p /data_dir/hostpkg && mkdir -p build_dir && ln -s /data_dir/hostpkg build_dir/hostpkg
+
 TMPFS=1 ./feeds/x/rom/lede/make.sh make -j$1 && sh upload.sh
 
 _EXIT=$?
 [ "x$_EXIT" = "x0" ] || {
+	sudo -E df -h
+	sudo du -sh bin build_dir/* staging_dir/*
 	make V=s >>make.log 2>&1
 	exit $_EXIT
 }
