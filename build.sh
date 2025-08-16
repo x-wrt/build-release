3d60cfc5e09fa8e2ad83860e3a9f5b21500a237f
Chen Minqiang <ptpt52@gmail.com>
2025-08-17 03:53:04 +0800

    release: v25.04_b202508162121
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

--- build.sh
+++ build.sh
@@ -22,7 +22,11 @@ cd pigz && \
 make && \
 sudo -E cp pigz /bin/pigz && sudo -E ln -sf pigz /bin/unpigz)
 sleep 1
+git config user.email "dev@x-wrt.com"
+git config user.name "Developer X"
+sleep 1
 wget https://github.com/openwrt/openwrt/pull/19792.patch && git am 19792.patch
+sleep 1
 
 mkdir .build_x
 echo CONFIG_VERSION_NUMBER=\"`cat release.tag`\" >.build_x/env
